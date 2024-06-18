fcc62d653e1a65150fb6e0f509eac3e198761155
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2024-06-18 13:51:10 +0530

    learning list

--- lists_forms_keys/src/App.js
+++ lists_forms_keys/src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
+// --------------LISTS--------------------
+const arr = [11,22,33,44,55,66];//Creating list
 function App() {
-  return (
-   <h1>Hello World</h1>
+  return ( //rendering list
+    <div>
+      <h1>Lists in React</h1>
+   <ul>
+    {arr.map((item)=>(
+      <li>{item}</li> //traversing list
+    ))}
+   </ul>
+   </div>
     
   );
 }
