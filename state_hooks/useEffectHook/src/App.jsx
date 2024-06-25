f1e4a9e6cfe351340087bac616c4a5a4d534bb86
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2024-06-25 10:57:50 +0530

    unmount using useEffect

--- state_hooks/useEffectHook/src/App.jsx
+++ state_hooks/useEffectHook/src/App.jsx
@@ -1,15 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import MyComponent from "./MyComponent";
 
 function App(){
+
+  const [isVisible, setVisible] = useState(true);
 
   useEffect(()=>{
     console.log("App component is mounting....");
   }, []);
 
   return <div className="App">
-    <MyComponent />
+    {isVisible ?< MyComponent /> : <></> } {/*shows MyComponent only when isVisible is true else it shows nothing on screen and unmounts the myComponent*/}
+    <button onClick={() => setVisible(!isVisible)}>Toggle</button>
   </div>
 }
 
 export default App;
+
+
+//unmounting using useEffect
+//
--- state_hooks/useEffectHook/src/MyComponent.jsx
+++ state_hooks/useEffectHook/src/MyComponent.jsx
@@ -1,13 +1,49 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 
-const MyComponent = () =>{
+//---------------useEffect() when mounting on screen----------------
+// const MyComponent = () =>{
+//     useEffect(()=>{
+//         console.log("My component is mouting...")
+//     }, [])
+
+//     return (
+//         <h3>My Component</h3>
+//     )
+
+//--------useEffect() when updating -----------------
+const MyComponent = ()=>{
+
+    const [count, setCount] = useState(0);
+    const [count2, setCount2] = useState(10);
+    
+
     useEffect(()=>{
-        console.log("My component is mouting...")
-    }, [])
+        console.log("MyComponent is mounting....")
+    }, []) //empty dependency tells that this needs to be run when mounting
+
+    useEffect(()=>{
+        console.log("Count got updated")
+    }, [count, count2]); //runs everytime when updating count
 
     return (
-        <h3>My Component</h3>
+        <div>
+            <p>Count is {count}</p>
+            <p>Count2 is {count2}</p>
+            <button onClick={ () => setCount( count + 1 )}>Press button</button>
+            <button onClick={ () => setCount2( count2 + 10 )}>Press button</button>
+        </div>
     )
 }
 
+
 export default MyComponent;
+
+
+
+
+
+
+
+// Notes 
+//useEffect when mounting on screen then leave dependencyArray empty
+//useEffect when updating on screen then add function variable in dependencyArray 
