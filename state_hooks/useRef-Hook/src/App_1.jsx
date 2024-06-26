4e094c03cfa2b959bacb7904ddc5d1fce856162a
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2024-06-26 09:58:07 +0530

    completed useRef()

--- state_hooks/useRef-Hook/src/App_1.jsx
+++ state_hooks/useRef-Hook/src/App_1.jsx
@@ -6,19 +6,13 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  // let a =0;
-
-  // useEffect(()=>{
-  //   a = a+1; // when we re-render the state the value of a again becomes 0 and value of a remains 1 on again re-rendering; so to overcome this problem useRef hook was introduced
-  //    console.log(`Rendering and value of a is ${a}....`);
-  // })
-
-  const a = useRef(0);
+  const ref = useRef();
 
   useEffect(()=>{
-    a.current = a.current+1;
-    console.log(`Rendering and value of a is ${a.current}...`);
-  })
+   
+    console.log(`First rendering ...`);
+    ref.current.style.backgroundColor = "red";
+  }, [])
 
   return (
     <>
@@ -32,7 +26,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button ref = {ref} onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
@@ -48,10 +42,3 @@ function App() {
 
 export default App
 
-
-//Notes 
-// whenever value of count(state) changes the component re-renders on screen using useEffect 
-//but when value of useRef is changed the component is not re-rendered on screen!!
-
-//Usecase 1: used to change value accross platform inside useEffect 
-//UseCase 2 : used to reduce ussage of DOM manupilation in React
