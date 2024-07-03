790b874916632cfda4ab7611b95a4aa3b32e0726
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2024-07-03 11:56:04 +0530

    completed
    completed studying concept of useCallback

--- state_hooks/useCallbackHook/src/App.jsx
+++ state_hooks/useCallbackHook/src/App.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
+import { useCallback } from "react";
 
 function App() {
-  const getAdjective = () =>{
-    return "GOOD NIGHT"
-  }
+  const getAdjective = useCallback(() => {
+    return "GOOD NIGHT";
+  }, []);
 
   return (
     <div>
-      <Navbar adjective={"good morning"} getAdjective = {getAdjective} />
+      <Navbar adjective={"good morning"} getAdjective={getAdjective} />
     </div>
   );
 }
 
 export default App;
+
+
+//useCallback
+// This allows us to isolate resource intensive functions so that they will not automatically run on every render.
+
+// The useCallback Hook only runs when one of its dependencies update.
+
+// This can improve performance.
--- state_hooks/useCallbackHook/src/Components/Navbar.jsx
+++ state_hooks/useCallbackHook/src/Components/Navbar.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import {memo} from "react"
 
-const Navbar = ({adjective}) =>{
+const Navbar = ({adjective, getAdjective}) =>{
     console.log("Navbar is rendered")
     return (
         <div>
         I am a {adjective} navbar
+        <button onClick={() => {getAdjective()}}>CHANGE ME</button>
         </div>
     )
 }
 
-export default Navbar;
+export default memo(Navbar);
+
+
+//memo : it is rendered only when props of the state are changed else it will not render again and again on reloading of the page
+// if memo is not used the state on reload will render the Navbar on each reload
