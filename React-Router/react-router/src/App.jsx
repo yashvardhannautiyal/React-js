7357667f67307ea20de772a69430e0970948def5
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2024-09-18 20:18:05 +0530

    use of helper function : createRoutesFromElements

--- React-Router/react-router/src/App.jsx
+++ React-Router/react-router/src/App.jsx
@@ -2,30 +2,48 @@ import { About } from "./pages/About";
 import { Contact } from "./pages/Contact";
 import { Home } from "./pages/Home";
 import { Movie } from "./pages/Movie";
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 
 
 const App = () =>{
-const router = createBrowserRouter( //array of objects
-  [
-    {
-      path : "/", //path defines the page url
-      element : <Home />
-    },
-    {
-      path : "/about",
-      element : <About />
-    },
-    {
-      path : "/movie",
-      element : <Movie />
-    },
-    {
-      path : "/contact",
-      element : <Contact />
-    }
-  ]
+  // ------------------New method-without helper function---------------------------- 
+// const router = createBrowserRouter( //array of objects
+//   [
+//     {
+//       path : "/", //path defines the page url
+//       element : <Home />
+//     },
+//     {
+//       path : "/about",
+//       element : <About />
+//     },
+//     {
+//       path : "/movie",
+//       element : <Movie />
+//     },
+//     {
+//       path : "/contact",
+//       element : <Contact />
+//     }
+//   ]
+// )
+
+
+//---------------------------------Traditional method : using helper function------------------------------
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>//we are required with a parent element always
+      <Route  path = "/"   element = {<Home />} />
+      <Route  path = "/about"   element = {<About />} />
+      <Route  path = "/movie"   element = {<Movie />} />
+      <Route  path = "/contact"   element = {<Contact />} />
+      
+    </Route>
+  )
 )
+
+
+
 
 
 
